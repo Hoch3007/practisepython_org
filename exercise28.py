3566951625e002d0b304e518ac553d4a45745a57
Ricarda <42298717+Hoch3007@users.noreply.github.com>
2019-02-15 09:15:27 +0100

    Create exercise28.py

---
+++ exercise28.py
@@ -0,0 +1,10 @@
+##### practicepython.org: Exercise 28
+
+def maximum(a, b, c):
+    
+    if a>b and a>c:
+        return a
+    elif c>a and c>b:
+        return c
+    else:
+        return b       
